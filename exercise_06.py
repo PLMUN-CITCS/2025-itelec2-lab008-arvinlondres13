b1f36132b90ef09df9c0fca9ecbcfc269baa4c33
arvinlondres13 <arvinlondres_bsit@plmun.edu.ph>
2025-02-22 14:33:14 +0800

    exercise_06.py

--- exercise_06.py
+++ exercise_06.py
@@ -3,6 +3,7 @@ temperature = 35
 
 if temperature > 30:
 
-    print("It's a hot day!")  # Indented 4 spaces
 
-git commit -m "Submitting Python Week 04 - La#boratory # 08"
+
+    
+    print("It's a hot day!")  # Indented 4 spaces
